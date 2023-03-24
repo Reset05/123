1fa6fd4da787c6fb0b0db4a464972969468a768c
Reset05 <121117025+Reset05@users.noreply.github.com>
2023-03-24 16:13:07 +0300

    fix style and add media

--- pages/db/db.php
+++ pages/db/db.php
@@ -3,7 +3,7 @@ define('DIR', __DIR__);
 $host = 'localhost';
 $db = 'catalog';
 $user = 'root';
-$pass = 'root';
+$pass = '';
 $charset = 'utf8';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
